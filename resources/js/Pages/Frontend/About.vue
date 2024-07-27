401b822ed55afe36e9a2780dc16c481211710a19
cedieredicio <cedieredicio616@gmail.com>
2024-07-27 15:03:19 +0800

    my first commit

---
+++ resources/js/Pages/Frontend/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <Frontend>
+        <Head title="About Page" />
+        <h1>This is {{ title }}</h1>
+    </Frontend>
+</template>
+
+<script setup>
+import Frontend from '@/Layouts/FrontendLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    title: String 
+});
+</script>
